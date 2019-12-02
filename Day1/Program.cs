fd0c076803476d35ece1f5903bafdb52a6f4926c
Velvárt András <andras.velvart@aerinx.com>
2019-12-02 23:05:51 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFuel = 0;
+            int mass = 0;
+            int fuel = 0;
+            var lines = File.ReadAllLines("input.txt");
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            foreach (var line in lines)
+            {
+                mass = int.Parse(line);
+                fuel = CalculateRecursiveFuel(mass);
+                totalFuel += fuel;
+            }
+
+            Console.WriteLine($"Total fuel: {totalFuel} - in {sw.Elapsed}");
+
+            sw.Restart();
+            Console.WriteLine($"Total fuel: {lines.Sum(line => CalculateRecursiveFuel(int.Parse(line)))} - in {sw.Elapsed}");
+
+            sw.Restart();
+            totalFuel = 0;
+
+            Parallel.ForEach(lines,  l => totalFuel += CalculateRecursiveFuel(int.Parse(l)));
+            Console.WriteLine($"Total fuel: {totalFuel} - in {sw.Elapsed}");
+        }
+
+        private static int CalculateFuel(int mass)
+        {
+            return (mass / 3) - 2;
+        }
+
+        private static int CalculateRecursiveFuel(int mass)
+        {
+            var fuel = CalculateFuel(mass);
+            var total = fuel;
+
+            while ((fuel = CalculateFuel(fuel)) > 0)
+            {
+                total += fuel;
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Day2
+{
+    class Program
+    {
+        private const int HCF = 99;
+        private const int ADD = 1;
+        private const int MUL = 2;
+
+
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("Input.txt");
+            int[] memory;
+            
+            for (int noun = 0; noun <= 99; noun++)
+                for (int verb = 0; verb <= 99; verb++)
+                {
+                    memory = ParseInput(text);
+                    memory[1] = noun;
+                    memory[2] = verb;
+                    Execute(memory);
+
+                    if (memory[0] == 19690720)
+                    {
+                        Console.WriteLine($"Got it! Result: {100*noun + verb}");
+                        return;
+                    }
+                }
+
+
+            //Console.WriteLine("Program finished. ");
+            //Dump(memory);
+        }
+
+        private static void Dump(int[] memory)
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine(String.Join(',', memory));
+        }
+
+        private static int[] ParseInput(string text)
+        {
+            int[] result = text.Split(',').Select(t => int.Parse(t)).ToArray();
+            return result;
+        }
+
+        private static void Execute(int[] memory)
+        {
+            int instructionPointer = 0;
+
+            while (memory[instructionPointer] != HCF)
+            {
+                switch (memory[instructionPointer])
+                {
+                    case ADD:
+                        memory[memory[instructionPointer + 3]] =
+                            memory[memory[instructionPointer + 1]] + memory[memory[instructionPointer + 2]];
+
+                        instructionPointer += 4;
+                        break;
+                    case MUL:
+                        memory[memory[instructionPointer + 3]] =
+                            memory[memory[instructionPointer + 1]] * memory[memory[instructionPointer + 2]];
+
+                        instructionPointer += 4;
+                        break;
+                    default: throw new InvalidOperationException($"Invalid instruction at {instructionPointer} : {memory[instructionPointer]}");
+                }
+                //Dump(memory);
+            }
+        }
+    }
+}
