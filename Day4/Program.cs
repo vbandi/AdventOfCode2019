73df66d8d0a8c15bf451f6cf4b5a780d5f1af3df
Velvárt András <andras.velvart@aerinx.com>
2019-12-04 22:59:07 +0100

    Linq

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -13,7 +13,7 @@ namespace Day4
         {
             Part1BruteForce();
             Part2BruteForce();
-            Part2GroupBy();
+            Part2Linq();
             ParallelFor();
         }
 
@@ -81,28 +81,19 @@ namespace Day4
             Console.WriteLine($"Result: {count}. It took {sw.ElapsedMilliseconds} ms.");
         }
 
-        private static void Part2GroupBy()
+        private static void Part2Linq() //slow but readable
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            var count = 0;
-            for (int i = 183564; i < 657475; i++)
-            {
-                var s = i.ToString(CultureInfo.InvariantCulture);
 
-                //verify double digits
-                if (s.GroupBy(c => c).Select(g => g.Count()).All(groupSize => groupSize != 2))  //10x slower!
-                    continue;
+            var result = Enumerable.Range(183564, 657475 - 183564)
+                .Select(i => i.ToString(CultureInfo.InvariantCulture))
+                .Count(s =>
+                    s.SequenceEqual(s.OrderBy(c => c)) &&
+                    s.GroupBy(c => c).Select(g => g.Count()).Any(groupSize => groupSize == 2)
+                );
 
-                ////verify monotonous increase
-                if (s[0] > s[1] || s[1] > s[2] || s[2] > s[3] || s[3] > s[4] || s[4] > s[5])
-                    continue;
-
-                //Console.WriteLine($"{i} is a valid password!");
-                count++;
-            }
-
-            Console.WriteLine($"Result: {count}. It took {sw.ElapsedMilliseconds} ms.");
+            Console.WriteLine($"Result: {result}. It took {sw.ElapsedMilliseconds} ms.");
         }
 
         private static object lck = new object();
