b79f82ed0f7f6183e779ec3467212a64a2cd1f53
Velvárt András <andras.velvart@aerinx.com>
2019-12-04 22:13:37 +0100

    Added groupby

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 
 namespace Day4
 {
@@ -11,6 +12,7 @@ namespace Day4
         {
             Part1BruteForce();
             Part2BruteForce();
+            Part2GroupBy();
         }
 
         private static void Part1BruteForce()
@@ -21,14 +23,15 @@ namespace Day4
             for (int i = 183564; i < 657475; i++)
             {
                 var s = i.ToString(CultureInfo.InvariantCulture);
-                
-                //verify double digits
-                if (s[0] != s[1] && s[1] != s[2] && s[2] != s[3] && s[3] != s[4] && s[4] != s[5])
-                    continue;
 
                 //verify monotonous increase
                 if (s[0] > s[1] || s[1] > s[2] || s[2] > s[3] || s[3] > s[4] || s[4] > s[5])
                     continue;
+
+                //verify double digits
+                if (s[0] != s[1] && s[1] != s[2] && s[2] != s[3] && s[3] != s[4] && s[4] != s[5])
+                    continue;
+
 
                 //Console.WriteLine($"{i} is a valid password!");
                 count++;
@@ -45,10 +48,6 @@ namespace Day4
             for (int i = 183564; i < 657475; i++)
             {
                 var s = i.ToString(CultureInfo.InvariantCulture);
-
-                //verify monotonous increase
-                if (s[0] > s[1] || s[1] > s[2] || s[2] > s[3] || s[3] > s[4] || s[4] > s[5])
-                    continue;
 
                 //verify double digits
                 var continuousSimilarDigits = 1;
@@ -69,12 +68,41 @@ namespace Day4
                 if (!has2ContinuousDigits && continuousSimilarDigits != 2)
                     continue;
 
+                ////verify monotonous increase
+                if (s[0] > s[1] || s[1] > s[2] || s[2] > s[3] || s[3] > s[4] || s[4] > s[5])
+                    continue;
+
                 //Console.WriteLine($"{i} is a valid password!");
                 count++;
             }
 
             Console.WriteLine($"Result: {count}. It took {sw.ElapsedMilliseconds} ms.");
         }
+
+        private static void Part2GroupBy()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var count = 0;
+            for (int i = 183564; i < 657475; i++)
+            {
+                var s = i.ToString(CultureInfo.InvariantCulture);
+
+                //verify double digits
+                if (s.GroupBy(c => c).Select(g => g.Count()).All(groupSize => groupSize != 2))  //10x slower!
+                    continue;
+
+                ////verify monotonous increase
+                if (s[0] > s[1] || s[1] > s[2] || s[2] > s[3] || s[3] > s[4] || s[4] > s[5])
+                    continue;
+
+                //Console.WriteLine($"{i} is a valid password!");
+                count++;
+            }
+
+            Console.WriteLine($"Result: {count}. It took {sw.ElapsedMilliseconds} ms.");
+        }
+
 
     }
 }
